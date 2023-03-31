0e0f6b15d394e7bc1c2ee025f104df2bf810d625
Jonatan Silva <115708859+JonSilv4@users.noreply.github.com>
2023-03-31 09:23:10 -0300

    Add files via upload

--- frontend/src/styles/global.js
+++ frontend/src/styles/global.js
@@ -2,20 +2,39 @@ import { createGlobalStyle } from "styled-components";
 
 const Global = createGlobalStyle`
 
-* {
-    margin: 0;
-    padding: 0:
-    font-family: 'Arial', Helvetica;
-}
+    * {
+        margin: 0;
+        padding: 0;
+        font-family: 'Varela', sans-serif;
+    }
 
+    header {
+        padding: 10px;
+        display: flex;
+        align-items: center;
+        background-color: #68bc41;
+        justify-content: space-between;
+        color: white;
+        width: 100%;
+        position: fixed;
+        margin-top: -60px;
+        width: 100vw;
+    }
 
-body {
-    width: 100vw;
-    height: 100vw;
-    display: flex;
-    justify-content: center;
-    background-color: #f2f2f2;
-}
+    h1 {
+        color: #000080;
+        margin-left: 20px;
+        
+    }
+
+    body {
+        width: 100vw;
+        margin-top: 60px;
+        height: 100vh;
+        display: flex;
+        justify-content: center;
+        background-color: #f2f2f2;
+    }
 `;
 
-export default Global
+export default Global;
